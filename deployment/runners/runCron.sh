d47b593d8772afd890cb8242560c9c6d622e75b5
Chalukya-Sentieo <chalukya@sentieo.com>
2022-07-08 18:20:16 +0530

    Changes to make cron work

---
+++ deployment/runners/runCron.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+crontab /etc/cron.d/crontab
+crond -f -d 9
--- src/cron/cron.py
+++ src/cron/cron.py
@@ -1,1 +1,6 @@
 print "Hello World from Cron !!!"
+from time import sleep
+
+for i in range(0, 10):
+    sleep(1)
+    print "Adding an extra line", i
